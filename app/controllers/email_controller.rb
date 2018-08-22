b9ea685abfe5381f22c72f36641a8d77c8b01a55
laetitia69 <titi-fernandes@hotmail.fr>
2018-08-22 17:58:16 +0200

    commit

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,4 +1,17 @@
 class EmailController < ApplicationController
-  def index
-  end
+  	attr_accessor :object, :body
+
+    def index
+    	@email = Email.new
+  		@emails = Email.all
+    end
+
+    def create 
+    	@email = params.require(:email).permit(:object, :body)
+    end
+
+    def show
+  		@email = Email.find(params[:id])
+    end
+
 end
